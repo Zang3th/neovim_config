e08738e818b28c04590c606a30e52d99039c0e17
Zang3th <tim.krueger1805@googlemail.com>
2024-05-23 18:01:14 +0200

    Modified lualine and added todo-comments

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,9 +1,24 @@
-return {'nvim-lualine/lualine.nvim',
-       requires = { 'nvim-tree/nvim-web-devicons', opt = true },
-       config = function()
-           require('lualine').setup {
-               options = { theme = 'gruvbox_dark' },
-               extensions = { 'neo-tree' } 
-           }
-       end
+return {
+    'nvim-lualine/lualine.nvim',
+    requires = {
+        'nvim-tree/nvim-web-devicons',
+        opt = true
+    },
+    config = function()
+        require('lualine').setup {
+            options = { theme = 'gruvbox_dark' },
+            extensions = { 'neo-tree' },
+            sections = {
+                lualine_c = {}
+            },
+            inactive_sections = {
+                lualine_x = {}
+            },
+            tabline = {
+                lualine_a = { 'hostname' },
+                lualine_b = { 'filename' },
+                lualine_c = { 'diagnostics' }
+            }
+        }
+    end
 }
