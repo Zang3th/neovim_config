dd98a48dac92c39fd8e8867a1ef5afb7611878cf
Zang3th <tim.krueger1805@googlemail.com>
2024-06-12 10:05:34 +0200

    Added surround-nvim and modified README

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # Neovim Config
 
-![Screenshot_001](res/Screenshot_001.png)
+## General stuff
 
-![Screenshot_002](res/Screenshot_002.png)
+- Just another neovim config
+- Modular plugin integration
+- Covers all the basics like *git-integration*, *auto-complete*, *fuzzy-finding* ...
+- Color scheme is *gruvbox_dark*
+
+## Great learning resources
+
+- [TJ DeVries](https://www.youtube.com/@teej_dv)
+- [Primegean](https://www.youtube.com/@ThePrimeTimeagen)
+- [Typecraft](https://www.youtube.com/@typecraft_dev)
+
+## Appearance
+
+![Screenshot_002-1](res/Screenshot_002-1.png)
+![Screenshot_002-2](res/Screenshot_002-2.png)
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,65 +1,65 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    requires = {
-        "nvim-tree/nvim-web-devicons",
-        opt = true,
-    },
-    event = { "BufReadPost", "BufNewFile" },
+	"nvim-lualine/lualine.nvim",
+	requires = {
+		"nvim-tree/nvim-web-devicons",
+		opt = true,
+	},
+	event = { "BufReadPost", "BufNewFile" },
 
-    config = function()
-        require("lualine").setup({
-            options = {
-                theme = "gruvbox_dark",
-                globalstatus = true,
-                refresh = {
-                    tabline = 100,
-                    statusline = 100,
-                },
-                always_divide_middle = true,
-            },
-            extensions = { "nvim-tree", "lazy", "mason", "trouble" },
-            tabline = {
-                lualine_a = {
-                    {
-                        "buffers",
-                        show_modified_status = true,
-                        mode = 2,
-                        max_length = vim.o.columns * 3 / 4,
-                        filetype_names = {
-                            NvimTree = "NvimTree",
-                        },
-                        symbols = {
-                            modified = " ",
-                            alternate_file = "",
-                            directory = "",
-                        },
-                    },
-                },
-                lualine_z = {
-                    {
-                        "tabs",
-                        mode = 0,
-                    },
-                },
-            },
-            winbar = {},
-            sections = {
-                lualine_a = {
-                    {
-                        "mode",
-                        mode = 2,
-                        path = 1,
-                    },
-                },
-                lualine_b = { "branch" },
-                lualine_c = { "diff" },
-                lualine_x = { "diagnostics" },
-                lualine_y = { "encoding" },
-                lualine_z = { "location" },
-            },
-            inactive_sections = {
-                lualine_x = {},
-            },
-        })
-    end,
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "gruvbox_dark",
+				globalstatus = true,
+				refresh = {
+					tabline = 100,
+					statusline = 100,
+				},
+				always_divide_middle = true,
+			},
+			extensions = { "nvim-tree", "lazy", "mason", "trouble" },
+			tabline = {
+				lualine_a = {
+					{
+						"buffers",
+						show_modified_status = true,
+						mode = 2,
+						max_length = vim.o.columns * 3 / 4,
+						filetype_names = {
+							NvimTree = "NvimTree",
+						},
+						symbols = {
+							modified = " ",
+							alternate_file = "",
+							directory = "",
+						},
+					},
+				},
+				lualine_z = {
+					{
+						"tabs",
+						mode = 0,
+					},
+				},
+			},
+			winbar = {},
+			sections = {
+				lualine_a = {
+					{
+						"mode",
+						mode = 2,
+						path = 1,
+					},
+				},
+				lualine_b = { "branch" },
+				lualine_c = { "diff" },
+				lualine_x = { "diagnostics" },
+				lualine_y = { "encoding" },
+				lualine_z = { "location" },
+			},
+			inactive_sections = {
+				lualine_x = {},
+			},
+		})
+	end,
 }
