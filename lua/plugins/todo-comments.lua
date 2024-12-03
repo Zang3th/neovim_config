a67a7c1446abbdf0ecb32aa2d3587f1b69ec7872
Zang3th <tim.krueger1805@googlemail.com>
2024-12-03 10:35:32 +0100

    Added 'done' keyword to todo-comments

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -6,6 +6,25 @@ return {
         signs = false,
     },
     config = function()
-        require("todo-comments").setup()
+        require("todo-comments").setup({
+            -- The default config is pretty complete already, just some little tweaks
+            keywords = {
+                TODO = { icon = " ", color = "info" },
+                DONE = { icon = " ", color = "done" },
+                NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+                WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+                FIX = {
+                  icon = " ",
+                  color = "error",
+                  alt = { "FIXME", "BUG", "FIXIT", "ISSUE", "ERROR", "ERR" },
+                },
+                HACK = { icon = " ", color = "warning" },
+                PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+                TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+            },
+            colors = {
+                done = { "#504945" },
+            },
+        })
     end,
 }
