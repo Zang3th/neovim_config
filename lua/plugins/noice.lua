96d468d8bb8dc637ed0e224b38968865ede3ac87
Zang3th <tim.krueger1805@googlemail.com>
2025-01-06 17:43:27 +0100

    Added manual notify config (managed by noice)

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -7,5 +7,27 @@ return {
     dependencies = {
         "MunifTanjim/nui.nvim",
         "rcarriga/nvim-notify",
+        "hrsh7th/nvim-cmp",
     },
+    config = function()
+        require("noice").setup({
+            require("notify").setup({
+                timeout = 5000,
+                stages = "slide",
+                render = "compact",
+                max_width = 60,
+                minimum_width = 60,
+                fps = 144,
+                background_color = "#000000",
+            }),
+            lsp = {
+                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                override = {
+                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                    ["vim.lsp.util.stylize_markdown"] = true,
+                    ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+                },
+            },
+        })
+    end,
 }
