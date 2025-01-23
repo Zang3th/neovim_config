ae17d326bb6f716aa23edc81b7ce9d65ffad06f9
Zang3th <tim.krueger1805@googlemail.com>
2025-01-23 19:24:11 +0100

    Changed some vimtex settings

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -5,6 +5,15 @@ return {
         vim.g.vimtex_view_general_viewer = "okular"
         vim.g.vimtex_view_general_options = '--unique file:@pdf\\#src:@line@tex'
         vim.g.vimtex_mapping_enabled = 0
+
+        vim.g.vimtex_compiler_method = 'latexmk'
+        vim.g.vimtex_compiler_latexmk_engines = {
+            ['_'] = '-xelatex', -- Default
+            pdflatex = '-pdf',
+            lualatex = '-lualatex',
+            xelatex = '-xelatex',
+        }
+
         local keymap = require("keymaps")
         keymap.vimtex_keymaps()
     end,
