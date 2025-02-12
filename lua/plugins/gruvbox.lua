9e16725c38afe1509afbbebc565167d5f4c6409a
Zang3th <tim.krueger1805@googlemail.com>
2025-02-12 09:42:43 +0100

    Changed some markdown colors

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -21,13 +21,14 @@ return {
             invert_tabline = false,
             invert_intend_guides = false,
             inverse = true, -- invert background for search, diffs, statuslines and errors
-            contrast = "", -- can be "hard", "soft" or empty string
+            contrast = "",  -- can be "hard", "soft" or empty string
             palette_overrides = {},
             overrides = {
-                ["@text.emphasis"] = { fg = "#83a598" }, -- Blue
-                ["@macro"] = { fg = "#ebdbb2", bold = true }, -- FG
-                ["@lsp.type.class.cpp"] = { fg = "#8ec07c" }, -- Aqua
-                ["@lsp.type.struct.cpp"] = { fg = "#8ec07c" }, -- Aqua
+                ["@markup.strong"]       = { fg = "#83a598", bold = true }, -- Markdown bold in Blue/bold
+                ["@text.emphasis"]       = { fg = "#d3869b" },              -- Markdown italic in Purple
+                ["@macro"]               = { fg = "#ebdbb2", bold = true }, -- C/C++ macro in FG/bold
+                ["@lsp.type.class.cpp"]  = { fg = "#8ec07c" },              -- C++ class in Aqua
+                ["@lsp.type.struct.cpp"] = { fg = "#8ec07c" },              -- C/C++ struct in Aqua
             },
             dim_inactive = false,
             transparent_mode = false,
