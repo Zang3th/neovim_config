de227c0fed88ba5c1c8bd72a034a6ee9b9e5dbb3
Zang3th <tim.krueger1805@googlemail.com>
2025-10-17 15:38:22 +0200

    Added neovide config

--- init.lua
+++ init.lua
@@ -1,3 +1,17 @@
+-- Neovide config
+if vim.g.neovide then
+    vim.o.guifont = "JetBrainsMono Nerd Font:h12"
+    vim.g.neovide_cursor_antialiasing = true
+    vim.g.neovide_scroll_animation_length = 0.1
+    vim.g.neovide_cursor_animation_length = 0
+    vim.g.neovide_floating_corner_radius = 0.0
+    vim.g.neovide_background_color = "#282828"
+    vim.g.neovide_title_background_color = "#282828"
+    vim.g.neovide_title_text_color = "#ebdbb2"
+    vim.g.neovide_refresh_rate = 120
+    vim.g.neovide_has_mouse_grid_detection = true
+end
+
 -- Install lazy package manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 ---@diagnostic disable-next-line: undefined-field
