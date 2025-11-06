dbbd41f4696986f712eac244a1a3a6d9014e859c
Zang3th <tim.krueger1805@googlemail.com>
2025-11-06 07:44:50 +0100

    CMake config only makes problems under windows

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,11 +6,7 @@ return {
             "nvim-lua/plenary.nvim",
             "nvim-tree/nvim-web-devicons",
             "folke/todo-comments.nvim",
-            {
-                "nvim-telescope/telescope-fzf-native.nvim",
-                build =
-                "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 && cmake --build build --config Release",
-            },
+            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
             "nvim-telescope/telescope-ui-select.nvim",
         },
         lazy = false,
