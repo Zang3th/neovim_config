ac1270c78a28373a16f929edcd641826f77eaad4
Zang3th <tim.krueger1805@googlemail.com>
2025-11-06 11:04:04 +0100

    Added dashboard

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,36 @@
+return {
+    "goolord/alpha-nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        local alpha = require("alpha")
+        local dashboard = require("alpha.themes.dashboard")
+
+        dashboard.section.header.val = {
+            "                                                     ",
+            "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+            "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+            "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+            "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+            "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+            "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+            "                                                     ",
+        }
+
+        dashboard.section.buttons.val = {
+            dashboard.button("n", "  > New file",  ":ene <BAR> startinsert <CR>"),
+            dashboard.button("f", "  > Find file", ":Telescope find_files<CR>"),
+            dashboard.button("r", "  > Recent",    ":Telescope oldfiles<CR>"),
+            dashboard.button("s", "  > Settings",  ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+            dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
+        }
+
+        alpha.setup(dashboard.opts)
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "alpha",
+            callback = function()
+                vim.opt_local.foldenable = false
+            end,
+        })
+    end,
+}
