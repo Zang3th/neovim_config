1b8791c6648dae5096fdb4b6c447f03c7428fa20
Zang3th <tim.krueger1805@googlemail.com>
2026-01-05 17:21:29 +0100

    Added code_action keybind

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -60,6 +60,7 @@ vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+vim.keymap.set("n", "ga", vim.lsp.buf.code_action, opts)
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 vim.keymap.set({ "n", "v" }, "<C-f><C-f>", vim.lsp.buf.format, opts)
 
