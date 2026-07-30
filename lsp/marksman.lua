66c11652fe0d04c24fda7232b32f0bcc07179ad4
Zang3th <tim.krueger1805@googlemail.com>
2026-07-30 14:39:58 +0200

    Fix for marksman crash in diffview

--- lsp/marksman.lua
+++ lsp/marksman.lua
@@ -3,6 +3,20 @@ return {
         "marksman",
     },
     filetypes = { "markdown" },
-    root_markers = { ".git" },
+
+    root_dir = function(bufnr, on_dir)
+        local name = vim.api.nvim_buf_get_name(bufnr)
+
+        -- Virtual diffview buffer shouldn't start the LSP
+        if name:match("^diffview://") then
+            return
+        end
+
+        local root = vim.fs.root(name, ".git")
+        if root then
+            on_dir(root)
+        end
+    end,
+
     capabilities = require("cmp_nvim_lsp").default_capabilities(),
 }
